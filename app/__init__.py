9b0afac81bd2e3cd0937eed460b43d95fc6786f0
Samyam Pradhan <pradhansamyam71@gmail.com>
2026-04-06 18:17:44 +0545

    Updated for mysql

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+import time
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,13 +8,29 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    # Temporary DB (we'll switch to MySQL later)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
+    DB_HOST = os.getenv("DB_HOST", "db")
+    DB_NAME = os.getenv("DB_NAME", "mytaskdb")
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
+
+    with app.app_context():
+        for i in range(10):
+            try:
+                db.create_all()
+                print("Database connected & tables ready")
+                break
+            except Exception as e:
+                print(f"Waiting for MySQL... retry {i}")
+                time.sleep(3)
 
     return app
